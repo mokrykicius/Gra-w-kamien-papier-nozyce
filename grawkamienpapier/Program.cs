6a5ba2167e74d7c80c165495f171dc4b28291c60
jilix <nagitosupermacy@gmail.com>
2024-10-17 12:14:18 +0200

    ifelse

--- grawkamienpapier/Program.cs
+++ grawkamienpapier/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Gra
+{
+    static void Main(string[] args)
+    {
+        Random rnd = new Random(); 
+        int wybor_komputera = rnd.Next(3);
+        Console.WriteLine("Witaj w grze kamień papier nożyce! Zmierz sie z naszym komputerem");
+        Console.WriteLine("Wybierz Kamień(k), papier(p), nożyce(n)");
+        string wybor_gracza = Console.ReadLine();
+        int wybor_gracza_int;
+        if (wybor_gracza == "k")
+        {
+            wybor_gracza_int = 0;
+        } 
+        else if (wybor_gracza == "p")
+        {
+            wybor_gracza_int = 1;
+        } 
+        else if (wybor_gracza == "n")
+        {
+            wybor_gracza_int = 2;
+        } 
+        else
+        {
+            Console.WriteLine("Zły wybór");
+        }
+        
+
+        
+    }
+}
