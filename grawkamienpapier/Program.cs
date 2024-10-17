8719b4095be114f7aa35f7361eae12a228ca50ac
jilix <nagitosupermacy@gmail.com>
2024-10-17 12:23:17 +0200

    switche

--- grawkamienpapier/Program.cs
+++ grawkamienpapier/Program.cs
@@ -2,30 +2,32 @@
 {
     static void Main(string[] args)
     {
-        Random rnd = new Random(); 
+        Random rnd = new Random();
         int wybor_komputera = rnd.Next(3);
         Console.WriteLine("Witaj w grze kamień papier nożyce! Zmierz sie z naszym komputerem");
         Console.WriteLine("Wybierz Kamień(k), papier(p), nożyce(n)");
         string wybor_gracza = Console.ReadLine();
         int wybor_gracza_int;
-        if (wybor_gracza == "k")
+        switch (wybor_gracza)
         {
-            wybor_gracza_int = 0;
-        } 
-        else if (wybor_gracza == "p")
-        {
-            wybor_gracza_int = 1;
-        } 
-        else if (wybor_gracza == "n")
-        {
-            wybor_gracza_int = 2;
-        } 
-        else
-        {
-            Console.WriteLine("Zły wybór");
+            case "k":
+                {
+                    wybor_gracza_int = 0;
+                    break;
+                }
+            case "p":
+                {
+                    wybor_gracza_int = 1;
+                    break;
+                }
+            case "n":
+                {
+                    wybor_gracza_int = 2;
+                    break;
+                }
+            default:
+                Console.WriteLine("Zła odp");
+                break;
         }
-        
-
-        
     }
 }
